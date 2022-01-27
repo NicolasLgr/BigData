cfa726f22780e16dfd29afd71a8960260e384b17
nicolaslgr <nico.leger99@laposte.net>
2022-01-27 21:13:13 +0100

    rendu final a 21h

--- TD2/td2.py
+++ TD2/td2.py
@@ -1,38 +1,63 @@
-import csv
 from os import listdir, times
 import random as rd
 import numpy as np
-import time
 
-tab = []
-tab_x = []
-tab_y = []
-mon_fichier = []
 
-# 47 normalement
+
+# 47 valeurs trouvé
 def get_result_from_csv(name, csv_file):
+    tab_x = []
+    tab_y = []
+    
     with open(csv_file, "r") as file:
         data = file.readlines()
+        #Pour chaque ligne du fichier csv on indique le separateur
         for line in data:
             line = line.split(",")
+            #On vérifie le nom de la personne
             if line[1] == name:
+                #On ne prend pas en compte les éléments vides ou retour à la ligne
                 if '' not in line and '\n' not in line:
-                    if float(line[2]).is_integer() == True and float(line[3]).is_integer() == True and float(line[4]).is_integer() == True and float(line[5]).is_integer() == True and float(line[6]).is_integer() == True and float(line[7]).is_integer() == True:
-                        if -1000 <= int(line[2]) <= 1000 and -1000 <= int(line[3]) <= 1000 and -1000 <= int(line[4]) <= 1000 and -1000 <= int(line[5]) <= 1000 and -1000 <= int(line[6]) <= 1000:
-                            if int(line[2]) != 0 and int(line[3]) != 0 and  int(line[4]) != 0 and int(line[5]) != 0 and int(line[6]) != 0 and int(line[7]) != 0:
-                                if -5000000 <= int(line[7]) <= 5000000:
-                                    line[7] = line[7].split("\n")[0]
-                                    tab_x.append([int(line[2]), int(line[3]), int(line[4]), int(line[5]), int(line[6])])
-                                    tab_y.append(int(line[7]))
+                    
+                    #On vérifie que les x soit des entiers
+                    if float(line[2]).is_integer() == True and float(line[3]).is_integer() == True:
+                        if float(line[4]).is_integer() == True and float(line[5]).is_integer() == True:
+                            if float(line[6]).is_integer() == True and float(line[7]).is_integer() == True:
+                                
+                                #On vérifie que les x soient entre -1000 et 1000
+                                if -1000 <= int(line[2]) <= 1000 and -1000 <= int(line[3]) <= 1000 and -1000 <= int(line[4]) <= 1000: 
+                                    if -1000 <= int(line[5]) <= 1000 and -1000 <= int(line[6]) <= 1000:
+                                        
+                                        #Le chiffre 0 apparaît trop souvent
+                                        #On concidère ça comme une incohérence
+                                        #On en prends pas en comlpte les lignes qui possèdent 0
+                                        if int(line[2]) != 0 and int(line[3]) != 0 and  int(line[4]) != 0:
+                                            if int(line[5]) != 0 and int(line[6]) != 0 and int(line[7]) != 0:
+                    
+                                                #On vérifie la borne de y
+                                                if -5000000 <= int(line[7]) <= 5000000:
+                                                    
+                                                    #On récupère uniquement la parti du y sans le \n
+                                                    line[7] = line[7].split("\n")[0]
+                                                    
+                                                    #Génère une liste des x et l'ajoute dans la liste de tous les x
+                                                    tab_x.append([int(line[2]), 
+                                                                int(line[3]), 
+                                                                int(line[4]), 
+                                                                int(line[5]), 
+                                                                int(line[6])])
+                                                    
+                                                    #Ajout du y dans la liste de tous les y
+                                                    tab_y.append(int(line[7]))
                                     
-    return len(tab), tab_x, tab_y
+    return len(tab_x), tab_x, tab_y
 
 tab_value = get_result_from_csv("Leger", "TD2\ExamColl.csv")
 
 # tu as un C
 # tu le multiplie par tous les X, tu additionne les multiplication et tu soustrais à y
 # Renvoie la meilleur distance entre 2 mots
-def get_coef_for_dictance(mon_y, mon_x, mon_c):
+def get_coef_for_distance(mon_y, mon_x, mon_c):
     resultat = 0
     for i in range(5):
         resultat = resultat + mon_c[i] * mon_x[i]
@@ -43,7 +68,7 @@ def get_distance(mes_y, mes_x, mon_c):
 
     final_distance = 0
     for i in range(len(mes_y)):
-        final_distance = final_distance + get_coef_for_dictance(mes_y[i], mes_x[i], mon_c)
+        final_distance = final_distance + get_coef_for_distance(mes_y[i], mes_x[i], mon_c)
 
     return final_distance
 
@@ -80,7 +105,7 @@ def crossover_nombre(mon_c1, mon_c2):
     # Il y a 85% de chance de prendre la lettre du premiers mot place en parametre
     for i in range(list_number1_length):
         choice = np.random.choice(np.arange(0, 2), 
-                                  p=[0.5, 0.5])
+                                  p=[0.55, 0.45])
         if choice == 0: 
             enfant.append(mon_c1[i])
         else: 
@@ -94,12 +119,12 @@ def new_generation(mon_y, mon_x, liste_de_c):
     #Ajoute le mot qui a la meilleur distance au tableau new_generation
     best_distance = get_best(mon_y, mon_x, liste_de_c)
     best_word = best_distance[0]
-    # print("best de la generation: ", best_word)
+    print("best de la generation: ", best_word)
     new_generation.append(best_word)
     liste_de_c.pop(liste_de_c.index(best_word))
 
     while len(new_generation) != len_mon_c:
-        random = np.random.choice(np.arange(0, 2), p=[0.4, 0.6])
+        random = np.random.choice(np.arange(0, 2), p=[0.8, 0.2])
         if random == 0:
             random_word1 = new_generation[0]
             random_word2 = rd.choice(liste_de_c)
@@ -123,42 +148,48 @@ def mutation(mon_y, mon_x, mon_c ):
     
     # Pour chaque caractere du mot donne en parametre
     for i in range(longueur_c):
-        chance = rd.randint(0,10)
+        chance = rd.randint(0,3)
         if chance == 0:
-            poids_mutation = 10
-            # if distance < 100:
-            #     poids_mutation = (distance // 10) - 1
-            #     if poids_mutation == 0:
-            #         poids_mutation = 1
+            poids_mutation = distance //10000
+            
             random_number = rd.randint(-1 * poids_mutation, poids_mutation)
-            # if -1000 > random_number + mon_c[i] > 1000:
-            #     calcul = (random_number + mon_c[i])
-            #     mon_c[i] = calcul % 1000
-            # else:
-            mon_c[i] = (random_number + mon_c[i])
+            if -1000 > (random_number + mon_c[i]) > 1000:
+                tmp = (random_number + mon_c[i])
+                mon_c[i] = tmp % 1000
+            else:
+                mon_c[i] = (random_number + mon_c[i])
+    
     return mon_c
 
 
 
 def algo_genetique(mon_y, mon_x, nb_iteration):
-    print("ma_super_target : ",mon_y)
+    list_of_c = word_list_init(300,5)
 
-    list_of_c = word_list_init(200,5)
-
-    print("iteration n" ,nb_iteration)
+    print("=== Generation numero :" ,nb_iteration,'===')
     new_gene_list = new_generation(mon_y, mon_x, list_of_c)
     best_word = get_best(mon_y, mon_x, new_gene_list)[0]
     best_distance = get_best(mon_y, mon_x, new_gene_list)[1]
-    print("le meilleur mot est :", best_word,"avec une distance de:",best_distance) 
+    print("le meilleur mot est :", best_word,"avec une distance de:",best_distance,"\n")
 
     while best_distance != 0:
         nb_iteration += 1
-        print("iteration n",nb_iteration)
+        print("=== Generation numero :" ,nb_iteration,'===')
         new_gene_list = new_generation(mon_y,mon_x, new_gene_list)
         best_word = get_best(mon_y,mon_x, new_gene_list)[0]
         best_distance = get_best(mon_y, mon_x, new_gene_list)[1]
         print("     le meilleur mot est :", best_word,"avec une distance de:",best_distance)
         print("")
+    print("\n=============RESULTAT=============")
+
+    return best_word, nb_iteration
 
 
 print(algo_genetique(tab_value[2], tab_value[1], 0))
+# moyenne_iteration = 0
+# for i in range(10):
+#     moyenne_iteration += algo_genetique(tab_value[2], tab_value[1], 0)[1]
+# print("=============MOYENNE=============\n",moyenne_iteration/i)
+
+# print("=============TABLEAU DE MON X=============\n",tab_value[1],"\n")
+# print("=============TABLEAU DE MON Y=============\n",tab_value[2])
